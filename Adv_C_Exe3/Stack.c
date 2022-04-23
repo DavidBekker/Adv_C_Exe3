c4a9b1b9b22b790cc99ce93277d41c92dd535c10
בקר בקר <david.bekker@ruppin365.net>
2022-04-23 23:43:49 +0300

    Strack final

--- Adv_C_Exe3/Stack.c
+++ Adv_C_Exe3/Stack.c
@@ -1,46 +1,165 @@
 #include "Stack.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define _CRT_SECURE_NO_WARNINGS 
 
 /***************** Stack ADT Implementation *****************/
 
 void initStack(Stack* s)
 {
-	// add your code here
+	s->head = NULL;
 }
 
 void destroyStack(Stack* s)
 {
-	// add your code here
+	charNode* current = s->head;
+	charNode* next;
+	while (current)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	s->head = NULL;
+	printf("Stack is Empty\n");
 }
+
 
 void push(Stack* s, char data)
 {
-	// add your code here
+	charNode* newn = (charNode*)malloc(sizeof(charNode));
+	if (newn == NULL)
+		return;//Check if newn is empty
+
+	newn->data = data;
+	newn->next = s->head;
+	s->head = newn;
 }
 
 char pop(Stack* s)
 {
-	// add your code here
+	if (!isEmptyStack) return;
+	charNode* current = s->head;
+	char tm = s->head->data;
+	s->head = s->head->next;
+	free(current);
+	return tm;
 }
 
 
 int isEmptyStack(const Stack* s)
 {
-	// add your code here
+	if (s->head == NULL)
+		return 1;
+	else
+		return 0;
 }
 
 /*************** Functions using stacks - Implementation/definition **************************/
 
 void flipBetweenHashes(const char* sentence)
 {
-	// add your code here
+	char res;
+	Stack Original, Tmp1, Tmp2;
+	initStack(&Original); initStack(&Tmp1); initStack(&Tmp2);
+	int lean = (strlen(sentence) - 1);
+	while (lean > -1)
+	{
+		push(&Original, sentence[lean]);
+		lean--;
+	}
+
+	while (!(isEmptyStack(&Original)))
+	{
+		while (res = !'#')
+		{
+			res = pop(&Original);
+			push(&Tmp1, res);
+		}
+		push(&Tmp1, res);
+		while (res != '#')
+		{
+			if (isEmptyStack(&Original))
+				break;
+			res = pop(&Original);
+			push(&Tmp2, res);
+		}
+		push(&Tmp2, res);
+
+		while (!(isEmptyStack(&Tmp2)))
+			push(&Tmp1, pop(&Tmp2));
+	}
+
+	while (!(isEmptyStack(&Tmp1)))
+		push(&Original, pop(&Tmp1));
+
+	printf("The new string is : ");
+	while (!(isEmptyStack(&Original)))
+		printf("%c", pop(&Original));
+
 }
 
 int isPalindrome(Stack* s)
 {
-	// add your code here
+	if (!isEmptyStack(&s))
+		return 0;
+	Stack Top1, Top2;
+	initStack(&Top1); initStack(&Top2);
+	Top1.head = s->head;
+	Top2.head = s->head;
+	int count = Stackleanght(&Top1);
+
+	Top1.head = s->head;
+	for (count; count > 0; count--)
+	{
+		for (int i = count - 1; i > 0; i--)
+			Top2.head = Top2.head->next;
+		if ((Top1.head->data) != (Top2.head->data))
+			return 0;
+		Top1.head = Top1.head->next;
+		Top2.head = s->head;
+	}
+	return 1;
 }
 
 void rotateStack(Stack* s, int n)
 {
-	// add your code here
+	if (isEmptyStack(&s)) return;
+	Stack stk1, stk2, original;
+	original.head = s->head;
+	initStack(&stk1); initStack(&stk2);
+	int leanght = Stackleanght(&original);
+	if (n<0 || n>leanght)
+		return;
+
+	for (int i = 0; i < leanght - n; i++)
+		push(&stk1, pop(&original));
+
+	while (!isEmptyStack(&original))
+		push(&stk2, pop(&original));
+
+	while (!isEmptyStack(&stk1))
+		push(&original, pop(&stk1));
+
+	while (!isEmptyStack(&stk2))
+		push(&original, pop(&stk2));
+	s->head = original.head;
+}
+
+
+int Stackleanght(Stack* s)
+{
+	if (isEmptyStack(&s)) return;
+	Stack Top;
+	int count = 1;
+	initStack(&Top);
+	Top.head = s->head;
+	while (Top.head->next != NULL)
+	{
+		count++;
+		Top.head = Top.head->next;
+	}
+	Top.head = s->head;
+	return count;
 }
